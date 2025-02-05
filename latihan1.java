e3c97e7dc3858f091cfce113235d1549e48eb79f
Taufiq <taufiq.dkstp@gmail.com>
2025-02-05 09:18:29 +0700

    latihan 1 (paknadir)

---
+++ latihan1.java
@@ -0,0 +1,12 @@
+public class latihan1 {
+    public static void main(String[] args) {
+        
+     String namaSaya = "muhammad taufiqurrahman";
+    String kelas = "x pplg 1";
+    double beratBadan = 00;
+    System.out.println("nama saya =" +" "+ namaSaya);
+    System.out.println("kelas saya ="+" " + kelas);
+    System.out.print("berat badan saya : ");
+    System.out.println(beratBadan);
+}
+}
